a0f199dae7cbd03af96fea57ab489e417e67ee50
udawat04 <itzudawat@gmail.com>
2024-06-25 09:41:04 +0530

    changes in layout

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Outlet } from 'react-router-dom'
 import Nav1 from './Nav1'
 import Header from './Header'
+import { useAuth } from '../../context/AuthContext'
 
 const Layout = () => {
+  const {setAuthenticate} = useAuth();
+  const fetchMe = async ()=> {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      const response = await fetch("http://localhost:4001/login",{
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          Authorization:`Bearer ${accessToken}`,
+        },
+      });
+      const data = await response.json();
+      if(data.status===200) {
+        setAuthenticate(true);
+      }
+      else {
+        setAuthenticate(false);
+      }
+    }
+  };
+  useEffect(()=>{
+    fetchMe();
+  },[]);
   return (
     <div>
         <>
